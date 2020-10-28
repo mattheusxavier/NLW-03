86f18dd23e6c53007bcda9ce05459dd0e9052066
mattheusxavier <mattheus.x.a@gmail.com>
2020-10-28 16:59:44 -0300

    Finish API

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from 'express';
 import { getRepository, Index } from 'typeorm';
-
 import orphanageView from '../views/orphamages_view';
 import Orphanage from '../models/Orphanage';
+import * as Yup from 'yup';
 
 
 export default {
@@ -13,7 +13,7 @@ export default {
             relations: ['images']
         })
 
-        return response.json(orphanages)
+        return response.json(orphanageView.renderMany(orphanages))
     },
 
     async show(request: Request, response: Response){
@@ -46,8 +46,8 @@ export default {
         const images = requestImages.map(image => {
             return { path: image.filename }
         }) 
-    
-        const orphanage = orphanagesRepository.create({
+
+        const data = {
             name,
             latitude,
             longitude,
@@ -56,7 +56,28 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        }
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            )
         })
+
+        await schema.validate(data, {
+            abortEarly: false,
+        })
+    
+        const orphanage = orphanagesRepository.create(data)
     
         await orphanagesRepository.save(orphanage);
     
--- backend/src/views/orphamages_view.ts
+++ backend/src/views/orphamages_view.ts
@@ -1,4 +1,5 @@
 import Orphanage from '../models/Orphanage';
+import images_view from './images_view';
 
 export default {
     render(orphanage: Orphanage) {
@@ -10,7 +11,12 @@ export default {
             about: orphanage.about,
             instructions: orphanage.instructions,
             opening_hours: orphanage.opening_hours,
-            open_on_week: orphanage.open_on_weekends
+            open_on_week: orphanage.open_on_weekends,
+            images: images_view.renderMany(orphanage.images)
         }
+    },
+
+    renderMany(orphanage: Orphanage[]){
+        return orphanage.map(orphanage => this.render(orphanage))
     }
 }
